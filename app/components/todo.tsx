77274ffb8606c2e2bcf659bf1a2fc1f67d49bfab
zer0kola <zer0kola321@gmail.com>
2024-08-11 23:08:37 +0900

    react-query로 supabase 연동

--- app/components/todo.tsx
+++ app/components/todo.tsx
@@ -1,18 +1,50 @@
 "use client";
 
-import { Checkbox, IconButton } from "@material-tailwind/react";
+import { Checkbox, IconButton, Spinner } from "@material-tailwind/react";
+import { useMutation } from "@tanstack/react-query";
+import { deleteTodo, updateTodo } from "app/actions/todo-actions";
+import { queryClient } from "app/config/react-query-client-provider";
 import { useState } from "react";
 
-export default function Todo({ id, title, completed, ...props }) {
-  const [isCompleted, setIsCompleted] = useState(completed);
+export default function Todo({ todo }) {
+  const [isCompleted, setIsCompleted] = useState(todo.completed);
   const [isEditing, setIsEditing] = useState(false);
-  const [newTitle, setNewTitle] = useState(title);
+  const [newTitle, setNewTitle] = useState(todo.title);
+
+  const updateTodoQuery = useMutation({
+    mutationKey: ["updateTodo"],
+    mutationFn: () =>
+      updateTodo({
+        ...todo,
+        title: newTitle,
+        completed: isCompleted,
+      }),
+    onSuccess: () => {
+      setIsEditing(false);
+      queryClient.invalidateQueries({
+        queryKey: ["todoList"],
+      });
+    },
+  });
+
+  const deleteTodoQuery = useMutation({
+    mutationKey: ["deleteTodo"],
+    mutationFn: () => deleteTodo(todo.id),
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: ["todoList"],
+      });
+    },
+  });
 
   return (
     <div className="w-full flex items-center gap-1">
       <Checkbox
         checked={isCompleted}
-        onChange={(e) => setIsCompleted(e.target.checked)}
+        onChange={async (e) => {
+          setIsCompleted(e.target.checked);
+          await updateTodoQuery.mutate();
+        }}
         onPointerEnterCapture={undefined}
         onPointerLeaveCapture={undefined}
         crossOrigin={undefined}
@@ -23,23 +55,31 @@ export default function Todo({ id, title, completed, ...props }) {
           onChange={(e) => setNewTitle(e.target.value)}
           type="text"
           className="border-b border-gray-300 pb-1 flex-1"
-          defaultValue={title}
+          defaultValue={todo.title}
         />
       ) : (
-        <p className={`flex-1 ${isCompleted && "line-through"}`}>{title}</p>
+        <p className={`flex-1 ${isCompleted && "line-through"}`}>{todo.title}</p>
       )}
 
       {isEditing ? (
         <IconButton
-          onClick={() => setIsEditing(!isEditing)}
+          onClick={async () => {
+            updateTodoQuery.mutate();
+          }}
           placeholder={undefined}
           onPointerEnterCapture={undefined}
           onPointerLeaveCapture={undefined}>
-          <i className="fas fa-check" />
+          {updateTodoQuery.isPending ? (
+            <Spinner onPointerEnterCapture={undefined} onPointerLeaveCapture={undefined} />
+          ) : (
+            <i className="fas fa-check" />
+          )}
         </IconButton>
       ) : (
         <IconButton
-          onClick={() => setIsEditing(!isEditing)}
+          onClick={() => {
+            setIsEditing(true);
+          }}
           placeholder={undefined}
           onPointerEnterCapture={undefined}
           onPointerLeaveCapture={undefined}>
@@ -48,10 +88,15 @@ export default function Todo({ id, title, completed, ...props }) {
       )}
 
       <IconButton
+        onClick={async () => await deleteTodoQuery.mutate()}
         placeholder={undefined}
         onPointerEnterCapture={undefined}
         onPointerLeaveCapture={undefined}>
-        <i className="fas fa-trash" />
+        {deleteTodoQuery.isPending ? (
+          <Spinner onPointerEnterCapture={undefined} onPointerLeaveCapture={undefined} />
+        ) : (
+          <i className="fas fa-trash" />
+        )}
       </IconButton>
     </div>
   );
--- app/ui.tsx
+++ app/ui.tsx
@@ -2,14 +2,37 @@
 import { Button, Input } from "@material-tailwind/react";
 import Todo from "./components/todo";
 import { useState } from "react";
+import { useMutation, useQuery } from "@tanstack/react-query";
+import { createTodo, getTodoList } from "./actions/todo-actions";
 
 export default function UI() {
-  const [todos, setTodos] = useState([{ id: 1, title: "Learn React", completed: false }]);
+  const [searchInput, setSearchInput] = useState("");
+
+  const todoListQuery = useQuery({
+    queryKey: ["todoList"],
+    queryFn: () => getTodoList({ searchInput }),
+  });
+
+  const createTodoQuery = useMutation({
+    mutationKey: ["createTodo"],
+    mutationFn: () =>
+      createTodo({
+        title: "",
+        completed: false,
+        created_at: new Date().toISOString(),
+      }),
+    onSuccess: () => {
+      todoListQuery.refetch();
+    },
+  });
+
   return (
     <div className="w-2/3 mx-auto items-center gap-2 py-10 flex flex-col">
       <h1 className="text-xl"> TODO LIST</h1>
       <Input
         label="Search Todo"
+        value={searchInput}
+        onChange={(e) => setSearchInput(e.target.value)}
         placeholder="Search Todo"
         icon={<i className="fas fa-search" />}
         onPointerEnterCapture={undefined}
@@ -17,21 +40,14 @@ export default function UI() {
         crossOrigin={undefined}
       />
 
-      {todos.map((todo) => (
-        <Todo key={todo.id} {...todo} />
+      {todoListQuery.isLoading && <div>Loading...</div>}
+      {todoListQuery.data?.map((todo) => (
+        <Todo key={todo.id} todo={todo} />
       ))}
 
       <Button
-        onClick={() =>
-          setTodos([
-            ...todos,
-            {
-              id: todos.length + 1,
-              title: `Todo ${todos.length + 1}`,
-              completed: false,
-            },
-          ])
-        }
+        onClick={() => createTodoQuery.mutate()}
+        loading={createTodoQuery.isPending}
         placeholder={undefined}
         onPointerEnterCapture={undefined}
         onPointerLeaveCapture={undefined}>
